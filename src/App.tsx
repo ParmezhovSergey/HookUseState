288c841b39f57f57c8816bb1cea42c27cde2fc52
ParmezhovSergey <parmezhovsergey@mail.ru>
2022-05-18 21:49:12 +0300

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//import "./App.css";
+import style from './App.module.css'
 
-function App() {
+
+  const App = () => {
+  let [manCounter, manSetCounter] = useState(0);
+  let [womanCounter, womanSetCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.app}>
+      <div className={style.counter}>
+      <div className={style.man}>
+        <div >Man</div>
+        <div className={style.manCounter}>{manCounter}</div>
+        <button className={style.butt} onClick={()=> {
+          manSetCounter((actual)=> actual + 1)
+        }} >+</button>
+      </div>
+      
+      <div className={style.woman}>
+        <div>Woman</div>
+        <div className={style.manCounter}>{womanCounter}</div>
+        <button className={style.butt} onClick={()=>{
+          womanSetCounter((actual)=> actual +1)
+        }} >+</button>
+      </div>
+      </div>
+      
+      
+      <div className={style.button}>
+        <div> <button className={style.butt} onClick={()=> {
+        manSetCounter((actual)=> actual - 1)
+        womanSetCounter((actual)=> actual -1)
+      }} >-</button></div>
+        <div> <button className={style.reset} onClick={()=> {
+        manSetCounter(0)
+        womanSetCounter(0)
+      }} >reset</button></div>
+      
+     
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
